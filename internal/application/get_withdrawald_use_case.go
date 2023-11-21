57a468728816a7f0dd832f6881193846c94d8731
Nikolay Magritov <Nikolay.Magritov@bki-okb.ru>
2023-11-22 02:00:37 +0300

    bad

--- internal/application/get_withdrawald_use_case.go
+++ internal/application/get_withdrawald_use_case.go
@@ -1,6 +1,8 @@
 package application
 
 import (
+	"fmt"
+
 	"github.com/Nickolasll/gomart/internal/domain"
 	"github.com/google/uuid"
 )
@@ -14,5 +16,6 @@ func (u GetWithdrawals) Execute(userID uuid.UUID) ([]domain.Withdraw, error) {
 	if err != nil {
 		return withdrawals, err
 	}
+	fmt.Println(err.Error())
 	return withdrawals, nil
 }
